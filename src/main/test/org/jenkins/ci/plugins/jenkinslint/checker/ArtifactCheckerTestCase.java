a37d99d76bc326c7b81ba1e49e528351a9b9b2dc
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 00:50:02 +0100

    Added Artifact Checker Test Case

---
+++ src/main/test/org/jenkins/ci/plugins/jenkinslint/checker/ArtifactCheckerTestCase.java
@@ -0,0 +1,41 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.FreeStyleProject;
+import hudson.tasks.ArtifactArchiver;
+import hudson.tasks.JavadocArchiver;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * ArtifactChecker Test Case.
+ *
+ * @author Victor Martinez
+ */
+public class ArtifactCheckerTestCase {
+    private ArtifactChecker checker = new ArtifactChecker("ArtifactChecker", false, false);
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+    @Test public void testEmptyJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        assertFalse(checker.executeCheck(project));
+    }
+    @Test public void testPublisherJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.getPublishersList().add(new JavadocArchiver("",false));
+        assertFalse(checker.executeCheck(project));
+    }
+    @Test public void testEmptyArtifactPublisher() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.getPublishersList().add(new ArtifactArchiver("","",false));
+        assertTrue(checker.executeCheck(project));
+    }
+    @Test public void testArtifactPublisher() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.getPublishersList().add(new ArtifactArchiver("something","",false));
+        assertFalse(checker.executeCheck(project));
+    }
+}
