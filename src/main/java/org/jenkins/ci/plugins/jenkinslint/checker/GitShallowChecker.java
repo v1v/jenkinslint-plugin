c5c07dd3477564b5b845cb1095516bb71f45b26b
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 00:49:21 +0100

    Added GitShallow Clone checker

---
+++ src/main/java/org/jenkins/ci/plugins/jenkinslint/checker/GitShallowChecker.java
@@ -0,0 +1,36 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.Item;
+import hudson.model.Project;
+import hudson.plugins.git.GitSCM;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.GitSCMExtensionDescriptor;
+import hudson.plugins.git.extensions.impl.CloneOption;
+import hudson.util.DescribableList;
+import org.jenkins.ci.plugins.jenkinslint.model.AbstractCheck;
+
+/**
+ * @author Victor Martinez
+ */
+public class GitShallowChecker extends AbstractCheck {
+
+    public GitShallowChecker(final String name, final boolean found, final boolean ignored) {
+        super(name, found, ignored);
+    }
+
+    public boolean executeCheck(Item item) {
+        if (item instanceof Project) {
+            if (((Project) item).getScm() instanceof hudson.plugins.git.GitSCM) {
+                DescribableList<GitSCMExtension, GitSCMExtensionDescriptor> extensionsList = ((GitSCM) ((Project) item).getScm()).getExtensions();
+                boolean status = true;
+                for (GitSCMExtension extension : extensionsList) {
+                    if (extension instanceof hudson.plugins.git.extensions.impl.CloneOption && ((CloneOption) extension).isShallow()) {
+                        status = false;
+                    }
+                }
+                return status;
+            }
+        }
+        return false;
+    }
+}
