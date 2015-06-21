980746a076f2001b653f63bf14b146519837381c
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 11:18:48 +0100

    Added Null SCM checker

---
+++ src/main/java/org/jenkins/ci/plugins/jenkinslint/checker/NullSCMChecker.java
@@ -0,0 +1,26 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.Descriptor;
+import hudson.model.Item;
+import hudson.model.Project;
+import hudson.tasks.ArtifactArchiver;
+import hudson.tasks.Publisher;
+import hudson.util.DescribableList;
+import org.jenkins.ci.plugins.jenkinslint.model.AbstractCheck;
+
+/**
+ * @author Victor Martinez
+ */
+public class NullSCMChecker extends AbstractCheck {
+
+    public NullSCMChecker(final String name, final boolean found, final boolean ignored) {
+        super(name, found, ignored);
+    }
+
+    public boolean executeCheck(Item item) {
+        if (item instanceof Project) {
+            return (((Project) item).getScm() instanceof hudson.scm.NullSCM);
+        }
+        return false;
+    }
+}
