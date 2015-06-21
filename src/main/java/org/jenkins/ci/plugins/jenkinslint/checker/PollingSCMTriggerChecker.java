98f23f64e78327d101dd5a319e6cf2b48d349b5d
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 11:18:40 +0100

    Added Polling SCM checker

---
+++ src/main/java/org/jenkins/ci/plugins/jenkinslint/checker/PollingSCMTriggerChecker.java
@@ -0,0 +1,24 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.Item;
+import hudson.model.Project;
+import hudson.triggers.SCMTrigger;
+import jenkins.model.Jenkins;
+import org.jenkins.ci.plugins.jenkinslint.model.AbstractCheck;
+
+/**
+ * @author Victor Martinez
+ */
+public class PollingSCMTriggerChecker extends AbstractCheck {
+
+    public PollingSCMTriggerChecker(final String name, final boolean found, final boolean ignored) {
+        super(name, found, ignored);
+    }
+
+    public boolean executeCheck(Item item) {
+        if (item instanceof Project) {
+            return (((Project) item).getTrigger(SCMTrigger.class) != null);
+        }
+        return false;
+    }
+}
