3defaab5156c6ced7e05b8c4363f48665f01ce79
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 11:17:08 +0100

    Added Job LogRotator checker

---
+++ src/main/java/org/jenkins/ci/plugins/jenkinslint/checker/JobLogRotatorChecker.java
@@ -0,0 +1,45 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.Item;
+import hudson.model.Project;
+import jenkins.model.BuildDiscarder;
+import org.jenkins.ci.plugins.jenkinslint.model.AbstractCheck;
+import hudson.tasks.LogRotator;
+
+/**
+ * @author Victor Martinez
+ */
+public class JobLogRotatorChecker extends AbstractCheck{
+
+    private final int EMPTY = -1;
+
+    public JobLogRotatorChecker(final String name, final boolean found, final boolean ignored) {
+        super(name, found, ignored);
+    }
+
+    public boolean executeCheck(Item item) {
+        if (item instanceof Project) {
+            Project project = (Project) item;
+            if ( project.getBuildDiscarder() != null ||
+                 isLogRotatorOkConfigured(project.getBuildDiscarder())) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isLogRotatorOkConfigured (BuildDiscarder buildDiscarder) {
+        if (buildDiscarder instanceof LogRotator) {
+
+            LogRotator rotator = (LogRotator) buildDiscarder;
+
+            return (rotator.getArtifactDaysToKeep() > EMPTY ||
+                    rotator.getArtifactNumToKeep() > EMPTY ||
+                    rotator.getDaysToKeep() > EMPTY ||
+                    rotator.getNumToKeep() > EMPTY);
+        }
+        return false;
+    }
+}
