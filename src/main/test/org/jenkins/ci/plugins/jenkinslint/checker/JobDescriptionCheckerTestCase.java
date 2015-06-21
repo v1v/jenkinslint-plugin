2d284fcc8eaa40fdf2f3f46f32a577302c375197
Victor Martinez <Victor.Martinez@king.com>
2015-06-21 22:19:07 +0100

    Added JobDescription Checker Test Case

---
+++ src/main/test/org/jenkins/ci/plugins/jenkinslint/checker/JobDescriptionCheckerTestCase.java
@@ -0,0 +1,33 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.FreeStyleProject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * JobDescriptionChecker Test Case.
+ *
+ * @author Victor Martinez
+ */
+public class JobDescriptionCheckerTestCase {
+    private JobDescriptionChecker checker = new JobDescriptionChecker("JobDescriptionChecker", false, false);
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+    @Test public void testDefaultJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Test public void testEmptyJobName() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject("");
+        assertTrue(checker.executeCheck(project));
+    }
+    @Test public void testJobDescription() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.setDescription("Some Description");
+        assertFalse(checker.executeCheck(project));
+    }
+}
