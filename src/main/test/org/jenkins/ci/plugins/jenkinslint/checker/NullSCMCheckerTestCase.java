593aaa09763aec041d32a09b30ae69229d990149
Victor Martinez <Victor.Martinez@king.com>
2015-06-24 21:27:48 +0100

    Added Null SCM checker Test Case

---
+++ src/main/test/org/jenkins/ci/plugins/jenkinslint/checker/NullSCMCheckerTestCase.java
@@ -0,0 +1,35 @@
+package org.jenkins.ci.plugins.jenkinslint.checker;
+
+import hudson.model.FreeStyleProject;
+import hudson.plugins.git.extensions.GitSCMExtension;
+import hudson.plugins.git.extensions.impl.CleanCheckout;
+import hudson.plugins.git.extensions.impl.CloneOption;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * NullSCMChecker Test Case.
+ *
+ * @author Victor Martinez
+ */
+public class NullSCMCheckerTestCase {
+    private NullSCMChecker checker = new NullSCMChecker("NullSCMChecker", false, false);
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    @Test public void testEmptyJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Test public void testGitSCMJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        project.setScm(new hudson.plugins.git.GitSCM(""));
+        assertFalse(checker.executeCheck(project));
+    }
+}
