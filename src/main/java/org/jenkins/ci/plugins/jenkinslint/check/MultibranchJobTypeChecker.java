c33c29a6b43ee0b4cb53368c3719489fc72a7613
Victor Martinez <user.name>
2015-07-04 21:07:11 +0200

    Multibranch Project checker since it does poll and it's an bad practice

---
+++ src/main/java/org/jenkins/ci/plugins/jenkinslint/check/MultibranchJobTypeChecker.java
@@ -0,0 +1,20 @@
+package org.jenkins.ci.plugins.jenkinslint.check;
+
+import hudson.model.Item;
+import org.jenkins.ci.plugins.jenkinslint.model.AbstractCheck;
+
+/**
+ * @author Victor Martinez
+ */
+public class MultibranchJobTypeChecker extends AbstractCheck{
+
+    public MultibranchJobTypeChecker() {
+        super();
+        this.setDescription("In order to comply with the style guide, Jenkins projects must avoid whitespace.");
+        this.setSeverity("High");
+    }
+
+    public boolean executeCheck(Item item) {
+        return item.getClass().getName().endsWith("FreeStyleMultiBranchProject");
+    }
+}
