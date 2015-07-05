abc72947119ee089124ad40ca91a066d6967edf8
Victor Martinez <user.name>
2015-07-05 12:55:16 +0200

    Added testcases for MultibranchJobType check

---
+++ src/main/test/org/jenkins/ci/plugins/jenkinslint/check/MultibranchJobTypeCheckerTestCase.java
@@ -0,0 +1,38 @@
+package org.jenkins.ci.plugins.jenkinslint.check;
+
+import com.github.mjdetullio.jenkins.plugins.multibranch.FreeStyleMultiBranchProject;
+import hudson.maven.MavenModuleSet;
+import hudson.model.FreeStyleProject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * MultibranchJobTypeChecker Test Case.
+ *
+ * @author Victor Martinez
+ */
+public class MultibranchJobTypeCheckerTestCase {
+    private MultibranchJobTypeChecker checker = new MultibranchJobTypeChecker();
+
+    @Rule public JenkinsRule j = new JenkinsRule();
+
+    @Test public void testEmptyJob() throws Exception {
+        FreeStyleProject project = j.createFreeStyleProject();
+        assertFalse(checker.executeCheck(project));
+    }
+    @Test public void testEmptyFreeStyleMultiBranchJob() throws Exception {
+        FreeStyleMultiBranchProject project = new FreeStyleMultiBranchProject(j.jenkins,"test");
+        j.jenkins.add(project, "test");
+        assertTrue(checker.executeCheck(project));
+    }
+    @Test public void testControlComment() throws Exception {
+        FreeStyleMultiBranchProject project = new FreeStyleMultiBranchProject(j.jenkins,"test");
+        assertFalse(checker.isIgnored(project.getDescription()));
+        project.setDescription("#lint:ignored:" + checker.getClass().getSimpleName());
+        assertTrue(checker.isIgnored(project.getDescription()));
+    }
+}
