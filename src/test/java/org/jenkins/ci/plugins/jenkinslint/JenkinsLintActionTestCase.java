f4f9eb04c366a6baa14965fbf72e5b75109c853e
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-07-15 00:42:47 +0200

    Initial UI Test in order to detect whether jenkinslint plugin is able to generate some table information

---
+++ src/test/java/org/jenkins/ci/plugins/jenkinslint/JenkinsLintActionTestCase.java
@@ -0,0 +1,42 @@
+package org.jenkins.ci.plugins.jenkinslint;
+
+import com.gargoylesoftware.htmlunit.WebAssert;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.model.FreeStyleProject;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author victor.martinez.
+ */
+public class JenkinsLintActionTestCase {
+
+    private static final String EMPTY_TABLE = "<tbody id=\"jenkinsLintTableBody\"></tbody></table>";
+    private static final String URL = "jenkinslint";
+
+    @Rule
+    public JenkinsRule j = new JenkinsRule();
+    @Test
+    public void testEmptyJob() throws Exception {
+        HtmlPage page = j.createWebClient().goTo(URL);
+        WebAssert.assertTextPresent(page, "JenkinsLint");
+        assertTrue(page.getWebResponse().getContentAsString().contains(EMPTY_TABLE));
+    }
+    @Test
+    public void testJob() throws Exception {
+        String jobName = "JOB";
+        j.createFreeStyleProject(jobName);
+        HtmlPage page = j.createWebClient().goTo(URL);
+        //System.out.println(page.getWebResponse().getContentAsString());
+        WebAssert.assertTextPresent(page, "JenkinsLint");
+        assertFalse(page.getWebResponse().getContentAsString().contains(EMPTY_TABLE));
+        assertTrue(page.getWebResponse().getContentAsString().contains(jobTable(j.jenkins.getRootUrl(), jobName)));
+    }
+
+    private String jobTable (String url, String jobName){
+        return "<tbody id=\"jenkinsLintTableBody\"><tr><td><a href=\""+ url +"job/"+jobName+"/\" target=\"_blank\">"+jobName+"</a></td>";
+    }
+}
