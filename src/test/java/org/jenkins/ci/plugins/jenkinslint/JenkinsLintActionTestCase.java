9a0ab0abce0995f7948bfa4dd5fe871199e4c27c
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-07-15 20:08:49 +0200

    Fixed URL issues when running UI tests

--- src/test/java/org/jenkins/ci/plugins/jenkinslint/JenkinsLintActionTestCase.java
+++ src/test/java/org/jenkins/ci/plugins/jenkinslint/JenkinsLintActionTestCase.java
@@ -2,6 +2,7 @@ package org.jenkins.ci.plugins.jenkinslint;
 
 import com.gargoylesoftware.htmlunit.WebAssert;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import hudson.model.FreeStyleProject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.*;
@@ -29,15 +30,16 @@ public class JenkinsLintActionTestCase {
     @Test
     public void testJob() throws Exception {
         String jobName = "JOB";
-        j.createFreeStyleProject(jobName);
+        FreeStyleProject project = j.createFreeStyleProject(jobName);
         HtmlPage page = j.createWebClient().goTo(URL);
         //System.out.println(page.getWebResponse().getContentAsString());
+        //System.out.println("URL " + j.getURL().toString() + project.getUrl());
         WebAssert.assertTextPresent(page, "JenkinsLint");
         assertFalse(page.getWebResponse().getContentAsString().contains(EMPTY_TABLE));
-        assertTrue(page.getWebResponse().getContentAsString().contains(jobTable(j.jenkins.getRootUrl(), jobName)));
+        assertTrue(page.getWebResponse().getContentAsString().contains(jobTable(j.getURL().toString() + project.getUrl(), jobName)));
     }
 
     private String jobTable (String url, String jobName){
-        return "<tbody id=\"jenkinsLintTableBody\"><tr><td><a href=\""+ url +"job/"+jobName+"/\" target=\"_blank\">"+jobName+"</a></td>";
+        return "<tbody id=\"jenkinsLintTableBody\"><tr><td><a href=\""+ url +"\" target=\"_blank\">"+jobName+"</a></td>";
     }
 }
