bb790ad2efb3b01d017afba95d6195f26701c625
Victor Martinez <VictorMartinezRubio@gmail.com>
2017-02-24 21:12:56 +0000

    [JENKINS-42310] Added Test cases for the JobAssignedLabel Lint defect

--- src/test/java/org/jenkins/ci/plugins/jenkinslint/check/JobAssignedLabelCheckerTestCase.java
+++ src/test/java/org/jenkins/ci/plugins/jenkinslint/check/JobAssignedLabelCheckerTestCase.java
@@ -1,8 +1,11 @@
 package org.jenkins.ci.plugins.jenkinslint.check;
 
+import hudson.matrix.MatrixProject;
+import hudson.maven.MavenModuleSet;
 import hudson.model.FreeStyleProject;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertFalse;
@@ -27,6 +30,32 @@ public class JobAssignedLabelCheckerTestCase {
     }
     @Test public void testWithAssignedLabel() throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
+        j.createSlave("test",null);
+        project.setAssignedLabel(j.jenkins.getLabel("test"));
+        project.save();
+        assertFalse(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMavenModuleJob() throws Exception {
+        MavenModuleSet project = j.createMavenProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMavenDescription() throws Exception {
+        MavenModuleSet project = j.createMavenProject("WithoutSystem");
+        j.createSlave("test",null);
+        project.setAssignedLabel(j.jenkins.getLabel("test"));
+        project.save();
+        assertFalse(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixProject() throws Exception {
+        MatrixProject project = j.createMatrixProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixProjectDescription() throws Exception {
+        MatrixProject project = j.createMatrixProject("WithoutSystem");
         j.createSlave("test",null);
         project.setAssignedLabel(j.jenkins.getLabel("test"));
         project.save();
