8aca94377879188dbbc3e39b5fa8b0a34e54e654
Victor Martinez <VictorMartinezRubio@gmail.com>
2017-02-24 21:36:24 +0000

    [JENKINS-42310] Added Test cases for the CleanupWorkspace  Lint defect

--- src/test/java/org/jenkins/ci/plugins/jenkinslint/check/CleanupWorkspaceCheckerTestCase.java
+++ src/test/java/org/jenkins/ci/plugins/jenkinslint/check/CleanupWorkspaceCheckerTestCase.java
@@ -1,11 +1,14 @@
 package org.jenkins.ci.plugins.jenkinslint.check;
 
+import hudson.matrix.MatrixProject;
+import hudson.maven.MavenModuleSet;
 import hudson.model.FreeStyleProject;
 import hudson.plugins.ws_cleanup.PreBuildCleanup;
 import hudson.plugins.ws_cleanup.WsCleanup;
 import hudson.tasks.ArtifactArchiver;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertFalse;
@@ -39,6 +42,28 @@ public class CleanupWorkspaceCheckerTestCase {
         FreeStyleProject project = j.createFreeStyleProject();
         project.getBuildWrappersList().add(new PreBuildCleanup(null, true, "", ""));
         assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMavenModuleJob() throws Exception {
+        MavenModuleSet project = j.createMavenProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMavenCleanupWorkspacePublisher() throws Exception {
+        MavenModuleSet project = j.createMavenProject("WithPublisher");
+        project.getPublishersList().add(new WsCleanup(null, true, true, true, true, true, true, true, true, ""));
+        assertFalse(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixProject() throws Exception {
+        MatrixProject project = j.createMatrixProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixProjectCleanupWorkspacePublisher() throws Exception {
+        MatrixProject project = j.createMatrixProject("WithoutSystem");
+        project.getPublishersList().add(new WsCleanup(null, true, true, true, true, true, true, true, true, ""));
+        assertFalse(checker.executeCheck(project));
     }
     @Test public void testControlComment() throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
