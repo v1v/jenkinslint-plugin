41f434542d170df192fb9e03f2a9b6c56a73d6e2
Victor Martinez <VictorMartinezRubio@gmail.com>
2017-02-25 20:41:25 +0000

    [JENKINS-42310] Added Test cases for the NullSCM Lint defect

--- src/test/java/org/jenkins/ci/plugins/jenkinslint/check/NullSCMCheckerTestCase.java
+++ src/test/java/org/jenkins/ci/plugins/jenkinslint/check/NullSCMCheckerTestCase.java
@@ -1,8 +1,11 @@
 package org.jenkins.ci.plugins.jenkinslint.check;
 
+import hudson.matrix.MatrixProject;
+import hudson.maven.MavenModuleSet;
 import hudson.model.FreeStyleProject;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.Issue;
 import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertFalse;
@@ -24,6 +27,35 @@ public class NullSCMCheckerTestCase {
     }
     @Test public void testGitSCMJob() throws Exception {
         FreeStyleProject project = j.createFreeStyleProject();
+        project.setScm(new hudson.plugins.git.GitSCM(""));
+        assertFalse(checker.executeCheck(project));
+    }
+
+    @Issue("JENKINS-42310")
+    @Test public void testMavenModuleJob() throws Exception {
+        MavenModuleSet project = j.createMavenProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMavenGitSCM() throws Exception {
+        MavenModuleSet project = j.createMavenProject();
+        assertTrue(checker.executeCheck(project));
+        project.delete();
+        project = j.createMavenProject();
+        project.setScm(new hudson.plugins.git.GitSCM(""));
+        assertFalse(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixProject() throws Exception {
+        MatrixProject project = j.createMatrixProject();
+        assertTrue(checker.executeCheck(project));
+    }
+    @Issue("JENKINS-42310")
+    @Test public void testMatrixGitSCM() throws Exception {
+        MatrixProject project = j.createMatrixProject();
+        assertTrue(checker.executeCheck(project));
+        project.delete();
+        project = j.createMatrixProject();
         project.setScm(new hudson.plugins.git.GitSCM(""));
         assertFalse(checker.executeCheck(project));
     }
